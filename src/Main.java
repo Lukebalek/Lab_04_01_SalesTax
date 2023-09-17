9b0a505763a09fc1b7e47f8f02c7996364ebf38d
Lukebalek <Lukebalek23@gmail.com>
2023-09-17 18:20:46 -0400

    Sales Tax

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+    double doublePrice = 5;
+    double doubleTax = .05;
+    double doubleTotal = 0;
+
+    doubleTax = doublePrice * .05;
+    doubleTotal = doublePrice + doubleTax;
+System.out.println("The cost of your item is " + doublePrice + " with a 5% sales tax of "
+        + doubleTax + " the total amount you owe is " + doubleTotal);
+
+
     }
 }
